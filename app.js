818844b1c96529fd34f7fd0863f7e691349e8bfb
Ulisses Albuquerque <ulisses.albuquerque@securusglobal.com>
2018-05-29 15:44:37 +1000

    Refactored directory layout and removed nedb

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var crypto = require('crypto');
-var express = require('express');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var nedb = require('nedb');
-var path = require('path');
-var session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
 
-var index = require('./routes/index');
+const index = require(path.resolve(__dirname, 'app/routes'));
 
-var app = express();
-
-// setup database globally
-app.locals.db = new nedb({
-  filename: path.join(__dirname, 'acme-banking.nedb'),
-  autoload: true
-});
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.resolve(__dirname, 'app/public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'app/public')));
 
 // setup session middleware
 app.use(session({
