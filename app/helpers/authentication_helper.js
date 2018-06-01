05282a5714358408a58f66c009d0d5dd19a7d333
Ulisses Albuquerque <ulisses.albuquerque@securusglobal.com>
2018-06-01 14:38:06 +1000

    Fixed bad copy&paste for account lookup

--- app/helpers/authentication_helper.js
+++ app/helpers/authentication_helper.js
@@ -6,7 +6,7 @@ const db = require(path.resolve(__dirname, '../models'));
 module.exports = {
   authenticate: function(email, password) {
     return new Promise((resolve, reject) => {
-      db.model('user').findOne({ where: { email }}).then((user) => {
+      db.model('user').findOne({ where: { email: email } }).then((user) => {
         if (user && user.passwordSalt && user.passwordHash) {
           const hash = crypto.createHash('sha512');
           hash.update(password);
