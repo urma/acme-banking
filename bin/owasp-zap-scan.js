d771d2b7cc4b49e15a518830b8b7f27a6e09d567
Ulisses Albuquerque <ulisses.albuquerque@securusglobal.com>
2018-06-25 14:29:35 +1000

    Fix ZAP scan wrapper script

--- bin/owasp-zap-scan.js
+++ bin/owasp-zap-scan.js
@@ -11,35 +11,14 @@ const logger = new winston.Logger({
   ],
 });
 
-const stackId = crypto.randomBytes(16).toString('hex');
-const dockerStackDeployCommand = [ 'stack', 'deploy', stackId, '--compose-file',
-  path.join(__dirname, '../docker-compose-jenkins.yml') ];
-const dockerPsCommand = [ 'ps', '--format', '{{ json . }}', '--filter', 'ancestor=owasp/zap2docker-stable:latest' ];
+/* Check for Jenkins environment variables */
+if (!process.env.hasOwnProperty('BUILD_TAG')) {
+  logger.error('This script is meant to be executed from within a Jenkins build plan');
+  logger.error('If you know what you are doing, please populate the required environment');
+  logger.error('variables and run it again');
+  process.exit(1);
+}
 
-logger.info('Starting application stack');
-child_process.execFile('/usr/local/bin/docker', dockerStackDeployCommand, (err) => {
-  if (err) {
-    logger.error('Error while initialising stack', err);
-    process.exit(1);
-  }
-
-  logger.info('Obtaining OWASP ZAP docker container ID');
-  child_process.execFile('/usr/local/bin/docker', dockerPsCommand, (err, stdout) => {
-    if (err) {
-      logger.error('Error while obtaining OWASP ZAP container ID', err);
-      process.exit(1);
-    }
-
-    const zapContainer = JSON.parse(stdout.toString('utf8'));
-    const dockerExecCommand = [ 'exec', zapContainer.ID, 'zap-full-scan.py', '-t', 'http://webapp:3000/' ];
-
-    logger.info('Running OWASP ZAP scan against web application');
-    child_process.execFile('/usr/local/bin/docker', dockerExecCommand, (err, stdout, stderr) => {
-      logger.info('stdout:', stdout.toString('utf8'));
-      logger.info('stderr:', stderr.toString('utf8'));
-      child_process.execFile('/usr/local/bin/docker', [ 'stack', 'rm', stackId ], () => {
-        logger.info('Shutting down stack');
-      });
-    });
-  });
-});
+const stackId = process.env.BUILD_TAG;
+const deployStatus = child_process.spawnSync('docker', [ 'stack', 'deploy', stackId, '--compose-file',
+  path.join(__dirname, '../docker-compose-jenkins.yml') ]);
