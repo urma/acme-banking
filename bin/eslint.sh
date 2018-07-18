fe2c89df68267f087063960173bdf6d7c821cb30
NegarShabab <negar.shabab@gmail.com>
2018-07-18 16:10:23 +1000

    Ignore asking for jenkins variable outside the jenkins

--- bin/eslint.sh
+++ bin/eslint.sh
@@ -3,11 +3,9 @@
 # ESLint scanning
 
 # Check whether we are inside the Jeniks environment or not
-[ -z ${WORKSPACE} ] && echo 'WORKSPACE is not defined. Please provide a WORKSPACE:'
-read -r WORKSPACE
+[ -z "${WORKSPACE}" ] && echo 'WORKSPACE is not defined.' && exit
 
-[ -z ${BUILD_TAG} ] && echo 'BUILD_TAG is not defined. Please provide a BUILD_TAG:'
-read -r BUILD_TAG
+[ -z "${BUILD_TAG}" ] && echo 'BUILD_TAG is not defined.' && exit
 
 mkdir -p "${WORKSPACE}/reports/eslint"
 eslint --format html --output-file "${WORKSPACE}/reports/eslint/${BUILD_TAG}.html" .
