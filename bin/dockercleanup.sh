be7826ab827e8e723d5ea98a352bcc3a98552e49
NegarShabab <negar.shabab@gmail.com>
2018-07-19 11:31:22 +1000

    fix syntax error

--- bin/dockercleanup.sh
+++ bin/dockercleanup.sh
@@ -7,6 +7,8 @@
 for label in $(docker ps --filter "ancestor=${DOCKER_IMAGE_LATEST}" --format "{{ .Labels }}") ; do
   swarm=$(echo ${label} | tr ',' "\n" | grep 'com.docker.stack.namespace' | awk -F= '{ print $2; }')
   echo "Found swarm [${swarm}]"
+  docker swarm rm ${swarm}
+  sleep 5
 done
 
 
