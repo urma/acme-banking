4b345a1bb6a180b426ada5ce0ff0ceaa2424f147
NegarShabab <negar.shabab@gmail.com>
2018-07-19 11:24:33 +1000

    fix syntax error

--- bin/dockercleanup.sh
+++ bin/dockercleanup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Check whether there is any leftover docker stack from last build
-if [ -n "${DOCKER_IMAGE_LATEST}" ] then 
-	# Obtain all containers spawned from the latest version of our image
-	for label in $(docker ps --filter "ancestor=${DOCKER_IMAGE_LATEST}" --format "{{ .Labels }}") ; do
-	  swarm=$(echo ${label} | tr ',' "\n" | grep 'com.docker.stack.namespace' | awk -F= '{ print $2; }')
-	  echo "Found swarm [${swarm}]"
-	  docker rm ${swarm}
-	done
-fi
+[ -z "${DOCKER_IMAGE_LATEST}" ] && exit
+
+# Obtain all containers spawned from the latest version of our image
+for label in $(docker ps --filter "ancestor=${DOCKER_IMAGE_LATEST}" --format "{{ .Labels }}") ; do
+  swarm=$(echo ${label} | tr ',' "\n" | grep 'com.docker.stack.namespace' | awk -F= '{ print $2; }')
+  echo "Found swarm [${swarm}]"
+done
+
+
