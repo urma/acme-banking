0856c7c03bae3cd902f66a0aafc0f149a6787a2c
NegarShabab <negar.shabab@gmail.com>
2018-07-20 14:22:46 +1000

    Test old version of docker clean up

---
+++ bin/dockercleanup-old.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Obtain all containers spawned from the latest version of our image
+for stack in $(docker stack ls --format '{{.Name}}' | grep 'jenkins-DevSecOps-acme-banking') ; do
+  echo "Removing currently running stack: ${stack}"
+  docker stack rm ${stack}
+  sleep 5
+done
