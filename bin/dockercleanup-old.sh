13c397295946004f707feb27ac7179045f0a4003
NegarShabab <negar.shabab@gmail.com>
2018-07-20 14:39:13 +1000

    fix stack name

--- bin/dockercleanup-old.sh
+++ bin/dockercleanup-old.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Obtain all containers spawned from the latest version of our image
-for stack in $(docker stack ls --format '{{.Name}}' | grep 'jenkins-DevSecOps-acme-banking') ; do
+for stack in $(docker stack ls --format '{{.Name}}' | grep 'jenkins-acme-banking') ; do
   echo "Removing currently running stack: ${stack}"
   docker stack rm ${stack}
   sleep 5
