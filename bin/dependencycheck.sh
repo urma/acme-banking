606e01941ebd43f862df1125a3f44dbfcc9471fe
NegarShabab <negar.shabab@gmail.com>
2018-08-17 16:54:52 +1000

    Fix docker pull command

--- bin/dependencycheck.sh
+++ bin/dependencycheck.sh
@@ -6,4 +6,5 @@
 mkdir -p "${WORKSPACE}/reports/owasp-dependency-check"
 
 # Run OWASP Dependency Check Dockerfile
+docker pull owasp/dependency-check
 docker run --rm --volume ${WORKSPACE}:/src --volume "${WORKSPACE}/OWASP-Dependency-Check/data":/usr/share/dependency-check/data --volume "${WORKSPACE}/reports/owasp-dependency-check":/report owasp/dependency-check --scan /src/package.json --format "XML" --project "acme-banking" -o /report -n
