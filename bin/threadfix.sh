fc50fab8c7f9e2416e1df9c30a3e4f6bcd34c7a9
NegarShabab <negar.shabab@gmail.com>
2018-08-17 18:07:38 +1000

    Add owasp-dependency-check result to Threadfix

--- bin/threadfix.sh
+++ bin/threadfix.sh
@@ -4,3 +4,4 @@ java -jar /var/lib/jenkins/threadfix/tfcli.jar --set key ${THREADFIX_API_KEY}
 
 # Input the scan reports
 java -jar /var/lib/jenkins/threadfix/tfcli.jar -u 1 ${WORKSPACE}/reports/zap/${BUILD_TAG}.xml
+java -jar /var/lib/jenkins/threadfix/tfcli.jar -u 1 ${WORKSPACE}/reports/owasp-dependency-check/dependency-check-report.xml
