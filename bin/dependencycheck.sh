fdf2d6bb47701f2667257980fa6d22d2412e32c2
NegarShabab <negar.shabab@gmail.com>
2018-08-17 16:46:04 +1000

    Add OWASP Dependency Check

---
+++ bin/dependencycheck.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check whether we are inside the Jeniks environment or not
+[ -z "${WORKSPACE}" ] && echo 'WORKSPACE is not defined.' && exit
+
+mkdir -p "${WORKSPACE}/reports/owasp-dependency-check"
+
+# Run OWASP Dependency Check Dockerfile
+docker run --rm --volume ${WORKSPACE}:/src --volume "${WORKSPACE}/OWASP-Dependency-Check/data":/usr/share/dependency-check/data --volume "${WORKSPACE}/reports/owasp-dependency-check":/report owasp/dependency-check --scan /src/package.json --format "XML" --project "acme-banking" -o /report -n
