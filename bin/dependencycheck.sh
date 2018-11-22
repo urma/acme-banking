c206524e2ad1a2fb08839140f070230ca5f67bce
Ulisses Albuquerque <ulisses.albuquerque@securusglobal.com>
2018-11-22 11:51:33 +1100

    bugfix: Resolve issues with Depency Check dockerized scan

--- bin/dependencycheck.sh
+++ bin/dependencycheck.sh
@@ -3,13 +3,21 @@
 # Check whether we are inside the Jeniks environment or not
 [ -z "${WORKSPACE}" ] && echo 'WORKSPACE is not defined.' && exit
 
-mkdir -p "${WORKSPACE}/reports/owasp-dependency-check"
-chmod -R 777 "${WORKSPACE}/reports/owasp-dependency-check"
-
-mkdir -p "${WORKSPACE}/OWASP-Dependency-Check/data"
-chmod -R 777 "${WORKSPACE}/OWASP-Dependency-Check/data"
-
+# HACK: File permission issues between Docker volume and host
+# filesystem
+export REPORT_VOLUME="${WORKSPACE}/reports/owasp-dependency-check"
+mkdir -p "${REPORT_VOLUME}"
+chmod -R 777 "${REPORT_VOLUME}"
 
 # Run OWASP Dependency Check Dockerfile
 docker pull owasp/dependency-check
-docker run --rm --volume ${WORKSPACE}:/src --volume "${WORKSPACE}/OWASP-Dependency-Check/data":/usr/share/dependency-check/data --volume "${WORKSPACE}/reports/owasp-dependency-check":/report owasp/dependency-check --scan /src/package.json --format "XML" --project "acme-banking" -o /report -n
+docker run \
+  --rm \
+  --volume ${WORKSPACE}:/src \
+  --volume dependencydb:/usr/share/dependency-check/data \
+  --volume ${REPORT_VOLUME}:/report \
+  owasp/dependency-check \
+  --scan /src/package.json \
+  --format "ALL" \
+  --project "acme-banking" \
+  -o /report
