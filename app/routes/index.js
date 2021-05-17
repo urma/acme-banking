527a4d6154adeda3cacc9715d70f3e73b99c5e7c
Ulisses Albuquerque <ulisses.albuquerque@latitudefinancial.com>
2021-05-17 13:23:06 +1000

    Feature/rate limiting (#194)
    * Enable rate limiting for requests
    
    * Enable rate limiting for auth handlers

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ const csurf = require('csurf');
 const express = require('express');
 const moment = require('moment');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 
 const router = express.Router();
 
@@ -13,11 +14,19 @@ router.use(csrfHandler);
 // eslint-disable-next-line security/detect-non-literal-require
 const helpers = require(path.resolve(__dirname, '../helpers'));
 
+// apply rate limiter to all requests
+router.use(
+  new rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 10,
+  })
+);
+
 router.use(async (req, res, next) => {
   if (req.session && req.session.user) {
-    res.locals.user = await req.app.locals.db.User
-      .query()
-      .findById(req.session.user.id);
+    res.locals.user = await req.app.locals.db.User.query().findById(
+      req.session.user.id
+    );
   }
   res.locals.moment = moment;
   return next();
@@ -82,14 +91,17 @@ router.post('/deposit', csrfHandler, async (req, res) => {
     .$relatedQuery('accounts')
     .findById(req.body.account)
     .eager('transactions');
-  
-  account.$relatedQuery('transactions').insert({
-    timestamp: new Date().toISOString(),
-    operation: 'deposit',
-    amount: parseFloat(req.body.amount),
-  }).then(() => {
-    return res.redirect('/balance');
-  });
+
+  account
+    .$relatedQuery('transactions')
+    .insert({
+      timestamp: new Date().toISOString(),
+      operation: 'deposit',
+      amount: parseFloat(req.body.amount),
+    })
+    .then(() => {
+      return res.redirect('/balance');
+    });
 });
 
 router.get('/withdraw', async (req, res) => {
@@ -111,36 +123,42 @@ router.post('/withdraw', csrfHandler, async (req, res) => {
     .findById(req.body.account)
     .eager('transactions');
 
-  account.$relatedQuery('transactions').insert({
-    timestamp: new Date().toISOString(),
-    operation: 'withdrawal',
-    amount: parseFloat(req.body.amount),
-  }).then(() => {
-    res.redirect('/balance');
-  });
+  account
+    .$relatedQuery('transactions')
+    .insert({
+      timestamp: new Date().toISOString(),
+      operation: 'withdrawal',
+      amount: parseFloat(req.body.amount),
+    })
+    .then(() => {
+      res.redirect('/balance');
+    });
 });
 
 /* login form */
 router.get('/login', async (req, res) => {
   res.render('login', {
     _csrf: req.csrfToken(),
-    title: 'Log In'
+    title: 'Log In',
   });
 });
 
 /* login submission */
 router.post('/login', csrfHandler, async (req, res) => {
-  helpers.auth.authenticate(req.body.email, req.body.password).then((user) => {
-    req.session.user = user;
-    return res.redirect('/');
-  }).catch((err) => {
-    delete(req.session.user);
-    res.render('login', {
-      _csrf: req.csrfToken(),
-      title: 'Log In',
-      error: err,
+  helpers.auth
+    .authenticate(req.body.email, req.body.password)
+    .then((user) => {
+      req.session.user = user;
+      return res.redirect('/');
+    })
+    .catch((err) => {
+      delete req.session.user;
+      res.render('login', {
+        _csrf: req.csrfToken(),
+        title: 'Log In',
+        error: err,
+      });
     });
-  });
 });
 
 router.get('/account', async (req, res) => {
@@ -151,32 +169,33 @@ router.get('/account', async (req, res) => {
 });
 
 router.post('/account', csrfHandler, async (req, res) => {
-  helpers.auth.authenticate(req.session.user.email, req.body.current).then(async (user) => {
-    if (req.body.new !== req.body.confirm) {
+  helpers.auth
+    .authenticate(req.session.user.email, req.body.current)
+    .then(async (user) => {
+      if (req.body.new !== req.body.confirm) {
+        return res.render('account', {
+          _csrf: req.csrfToken(),
+          title: 'Account Settings',
+          error: 'New password and confirmation do not match',
+        });
+      }
+
+      const updatedUser =
+        await req.app.locals.db.User.query().patchAndFetchById(user.id, {
+          password: req.body.new,
+        });
+
       return res.render('account', {
         _csrf: req.csrfToken(),
         title: 'Account Settings',
-        error: 'New password and confirmation do not match',
+        success: 'Password was updated successfully',
+        user: updatedUser,
       });
-    }
-
-    const updatedUser = await req.app.locals.db.User
-      .query()
-      .patchAndFetchById(user.id, {
-        password: req.body.new
-      });
-
-    return res.render('account', {
-      _csrf: req.csrfToken(),
-      title: 'Account Settings',
-      success: 'Password was updated successfully',
-      user: updatedUser,
     });
-  });
 });
 
 router.get('/logout', async (req, res) => {
-  delete(req.session.user);
+  delete req.session.user;
   res.redirect('/');
 });
 
